dfb663c54c566afa3f8cb8e95d9cdb9fde3c185d
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-12 23:33:29 +0200

    feat(backend): create TokenService and add GenerateJwtToken

---
+++ backend/LeaveManagerAPI/Services/TokenService.cs
@@ -0,0 +1,48 @@
+﻿using LeaveManagerAPI.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace LeaveManagerAPI.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly IConfiguration configuration;
+        private readonly UserManager<User> userManager;
+        public TokenService(IConfiguration configuration, UserManager<User> userManager)
+        {
+            this.configuration = configuration;
+            this.userManager = userManager;
+        }
+
+        public async Task<string> GenerateJwtToken(User user)
+        {
+            var claims = new List<Claim>
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Id),
+            new Claim(JwtRegisteredClaimNames.UniqueName, user.UserName!),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+        };
+
+            var userRoles = await userManager.GetRolesAsync(user);
+
+            foreach (var role in userRoles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtSettings:SecurityKey"]!));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.UtcNow.AddHours(1),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
