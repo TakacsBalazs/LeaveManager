5e413db4439b686f8119bcb637eaedb383ed1934
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-12 23:37:27 +0200

    feat(backend): create IAuthService and add LoginAsync

---
+++ backend/LeaveManagerAPI/Services/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using LeaveManagerAPI.Common;
+using LeaveManagerAPI.Models.Requests;
+using LeaveManagerAPI.Models.Responses;
+
+namespace LeaveManagerAPI.Services
+{
+    public interface IAuthService
+    {
+        Task<Result<LoginResponse>> LoginAsync(UserLoginRequest request);
+    }
+}
