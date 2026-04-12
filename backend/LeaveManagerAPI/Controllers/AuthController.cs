7d944944642968fa1a1e48052ff909b9e503cc98
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-12 23:40:54 +0200

    feat(backend): create AuthController and add login route

---
+++ backend/LeaveManagerAPI/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using LeaveManagerAPI.Models.Requests;
+using LeaveManagerAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagerAPI.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService authService;
+
+        public AuthController(IAuthService authService)
+        {
+            this.authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] UserLoginRequest request)
+        {
+            var result = await authService.LoginAsync(request);
+            if(!result.IsSuccess)
+            {
+                return BadRequest(result.Errors);
+            }
+
+            return Ok(result.Data);
+        }
+
+    }
+}
