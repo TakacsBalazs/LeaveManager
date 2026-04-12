b8e8e8aa4f3bd1ecd43c6bbd7a8cb63d1252334b
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-12 23:03:16 +0200

    feat(backend): create AppDbContext and add user table on model creating

---
+++ backend/LeaveManagerAPI/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using LeaveManagerAPI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeaveManagerAPI.Data
+{
+    public class AppDbContext : IdentityDbContext<User>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base (options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<User>(entity =>
+            {
+                entity.Property(x => x.FullName).IsRequired().HasMaxLength(100);
+                entity.Property(x => x.CreatedAt).IsRequired().HasDefaultValueSql("GETUTCDATE()");
+            });
+        }
+    }
+}
