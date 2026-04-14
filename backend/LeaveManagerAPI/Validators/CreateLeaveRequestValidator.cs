690cd85ea7239c1a5340b5ecfbfddddc65806aad
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-14 17:24:26 +0200

    feat(backend): create CreateLeaveRequestValidator

---
+++ backend/LeaveManagerAPI/Validators/CreateLeaveRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using LeaveManagerAPI.Models.Requests;
+
+namespace LeaveManagerAPI.Validators
+{
+    public class CreateLeaveRequestValidator : AbstractValidator<CreateLeaveRequest>
+    {
+        public CreateLeaveRequestValidator()
+        {
+            RuleFor(x => x.Type).IsInEnum();
+            RuleFor(x => x.StartDate).NotEmpty().GreaterThanOrEqualTo(DateOnly.FromDateTime(DateTime.UtcNow));
+            RuleFor(x => x.EndDate).NotEmpty().GreaterThanOrEqualTo(x => x.StartDate);
+            RuleFor(x => x.Reason).MaximumLength(250);
+        }
+    }
+}
