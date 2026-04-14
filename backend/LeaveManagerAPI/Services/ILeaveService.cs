0e0b2a31c06c77c296d740a058706b5cf7f23517
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-14 14:01:48 +0200

    feat(backend): create ILeaveService and add GetDashboardAsync

---
+++ backend/LeaveManagerAPI/Services/ILeaveService.cs
@@ -0,0 +1,10 @@
+﻿using LeaveManagerAPI.Common;
+using LeaveManagerAPI.Models.Responses;
+
+namespace LeaveManagerAPI.Services
+{
+    public interface ILeaveService
+    {
+        Task<Result<DashboardResponse>> GetDashboardAsync(string userId);
+    }
+}
