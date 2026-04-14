847a0cd96b4f6a4803550a8f98794d69dc33c6a8
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-14 14:27:23 +0200

    feat(backend): create LeaveRequestController and add CreateLeaveRequest

---
+++ backend/LeaveManagerAPI/Controllers/LeaveRequestController.cs
@@ -0,0 +1,34 @@
+﻿using LeaveManagerAPI.Models.Requests;
+using LeaveManagerAPI.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace LeaveManagerAPI.Controllers
+{
+    [Route("api/leaverequests")]
+    [ApiController]
+    public class LeaveRequestController : ControllerBase
+    {
+        private readonly ILeaveService leaveService;
+
+        public LeaveRequestController(ILeaveService leaveService)
+        {
+            this.leaveService = leaveService;
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> CreateLeaveReuqest([FromBody] CreateLeaveRequest request)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier)!;
+            var result = await leaveService.CreateLeaveRequestAsnyc(request, userId);
+            if(!result.IsSuccess)
+            {
+                return BadRequest(result.Errors);
+            }
+            return Ok(new {message = "Successful create a leave request!" });
+        }
+    }
+}
