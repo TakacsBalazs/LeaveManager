bfaea6bd8b7777dc19dd6166134fa3e3df2d36a7
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-14 13:55:20 +0200

    feat(backend): create LeaveBalance model with LeaveType enum

---
+++ backend/LeaveManagerAPI/Models/LeaveBalance.cs
@@ -0,0 +1,27 @@
+﻿namespace LeaveManagerAPI.Models
+{
+    public enum LeaveType
+    {
+        AnnualLeave,
+        SickLeave,
+        Unpaid
+    }
+    public class LeaveBalance
+    {
+        public int Id { get; set; }
+
+        public string UserId { get; set; } = string.Empty;
+
+        public User User { get; set; }
+
+        public int Year { get; set; }
+
+        public LeaveType Type { get; set; }
+
+        public int TotalDays { get; set; }
+
+        public int UsedDays { get; set; }
+
+        public int RemainingDays => TotalDays - UsedDays;
+    }
+}
