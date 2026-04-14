dd63d83b350b900bb6e92dabfce32566678bb2ce
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-14 17:22:54 +0200

    feat(backend): create UserLoginRequestValidator

---
+++ backend/LeaveManagerAPI/Validators/UserLoginRequestValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using LeaveManagerAPI.Models.Requests;
+
+namespace LeaveManagerAPI.Validators
+{
+    public class UserLoginRequestValidator : AbstractValidator<UserLoginRequest>
+    {
+        public UserLoginRequestValidator() { 
+            RuleFor(x => x.Email).NotEmpty();
+            RuleFor(x => x.Password).NotEmpty();
+        }
+    }
+}
