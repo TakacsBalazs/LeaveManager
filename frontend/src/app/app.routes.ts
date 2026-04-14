8f0f4fa676517d7a7513f4f2c9f9a87a402c55e1
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-14 16:35:28 +0200

    feat(frontend): add app/request-leave route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { LoginComponent } from './pages/login/login.component';
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 import { authGuard } from './core/guards/auth.guard';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { RequestLeaveComponent } from './pages/request-leave/request-leave.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
     { path: 'app', component: MainLayoutComponent, canActivate: [authGuard], children: [
         { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-        { path: 'dashboard', component: DashboardComponent }
+        { path: 'dashboard', component: DashboardComponent },
+        { path: 'request-leave', component: RequestLeaveComponent}
     ]}
 ];
