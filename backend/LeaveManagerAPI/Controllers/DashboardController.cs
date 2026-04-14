6646ce6d3234d142c25242b98c5cb5457319760a
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-14 14:04:10 +0200

    feat(backend): create DashboardController and add GetDashboardAsync

---
+++ backend/LeaveManagerAPI/Controllers/DashboardController.cs
@@ -0,0 +1,34 @@
+﻿using LeaveManagerAPI.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace LeaveManagerAPI.Controllers
+{
+    [Route("api/dashboard")]
+    [ApiController]
+    public class DashboardController : ControllerBase
+    {
+        private readonly ILeaveService leaveService;
+        public DashboardController(ILeaveService leaveService)
+        {
+            this.leaveService = leaveService;
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> GetDashboardAsync()
+        {
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier)!;
+            var result = await leaveService.GetDashboardAsync(userId);
+
+            if(!result.IsSuccess)
+            {
+                return BadRequest(result.Errors);
+            }
+
+            return Ok(result.Data);
+        }
+    }
+}
