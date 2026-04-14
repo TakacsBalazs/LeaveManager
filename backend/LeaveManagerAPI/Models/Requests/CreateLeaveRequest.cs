2cb8e98e726deecbf20dca21e4496b55b6e6e5e2
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-14 14:22:29 +0200

    feat(backend): create CreateLeaveRequest

---
+++ backend/LeaveManagerAPI/Models/Requests/CreateLeaveRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace LeaveManagerAPI.Models.Requests
+{
+    public class CreateLeaveRequest
+    { 
+
+        public LeaveType Type { get; set; }
+
+        public DateOnly StartDate { get; set; }
+
+        public DateOnly EndDate { get; set; }
+
+        public string? Reason { get; set; }
+
+    }
+}
