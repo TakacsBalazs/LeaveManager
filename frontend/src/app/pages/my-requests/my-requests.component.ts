afeaef342183df2271a1fc53d21c41a7034f5ecd
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-17 14:15:21 +0200

    feat(frontend): create and style the my-requests page

---
+++ frontend/src/app/pages/my-requests/my-requests.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { LeaveService } from '../../core/services/leave.service';
+import { LeaveRequestDto } from '../../models/leave';
+
+@Component({
+  selector: 'app-my-requests',
+  imports: [],
+  templateUrl: './my-requests.component.html',
+  styleUrl: './my-requests.component.scss'
+})
+export class MyRequestsComponent implements OnInit{
+
+  data: LeaveRequestDto[] = [];
+  isLoading = true;
+
+  constructor(private leaveService: LeaveService) {}
+
+  ngOnInit(): void {
+    this.leaveService.getMyLeaveRequests().subscribe({
+      next: (data) => {
+        this.data = data;
+        this.isLoading = false;
+      }
+    });
+  }
+
+}
