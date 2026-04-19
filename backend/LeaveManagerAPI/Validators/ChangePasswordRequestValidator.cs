6965028b19266e9ee45c0ad132ff9f8317b0c788
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-19 21:23:01 +0200

    feat(backend): create ChangePasswordRequestValidator

---
+++ backend/LeaveManagerAPI/Validators/ChangePasswordRequestValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using LeaveManagerAPI.Models.Requests;
+
+namespace LeaveManagerAPI.Validators
+{
+    public class ChangePasswordRequestValidator : AbstractValidator<ChangePasswordRequest>
+    {
+        public ChangePasswordRequestValidator()
+        {
+            RuleFor(x => x.OldPassword).NotEmpty();
+            RuleFor(x => x.NewPassword).NotEmpty().MinimumLength(6);
+            RuleFor(x => x.ConfirmNewPassword).NotEmpty().Equal(x => x.NewPassword);
+        }
+    }
+}
