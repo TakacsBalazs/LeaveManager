3e6f7aebd1f03fc6ed062cbd0dfc08c48bade6ed
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-19 19:25:36 +0200

    feat(backend): create UpdateUserRequestValidator

---
+++ backend/LeaveManagerAPI/Validators/UpdateUserRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using LeaveManagerAPI.Constants;
+using LeaveManagerAPI.Models.Requests;
+
+namespace LeaveManagerAPI.Validators
+{
+    public class UpdateUserRequestValidator : AbstractValidator<UpdateUserRequest>
+    {
+        public UpdateUserRequestValidator() {
+            RuleFor(x => x.Email).NotEmpty().EmailAddress();
+            RuleFor(x => x.Fullname).NotEmpty();
+            RuleFor(x => x.Roles).NotEmpty();
+            RuleForEach(x => x.Roles).Must(role => UserRoles.IsRoleExist(role)).WithMessage("Invalid role!");
+        }
+    }
+}
