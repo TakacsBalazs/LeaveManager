0102ba49ec7b7f57b555e07b64c3fb38ec3f7c2f
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-19 21:23:30 +0200

    feat(backend): add ChangePasswordAsync to IAuthService

--- backend/LeaveManagerAPI/Services/IAuthService.cs
+++ backend/LeaveManagerAPI/Services/IAuthService.cs
@@ -7,5 +7,7 @@ namespace LeaveManagerAPI.Services
     public interface IAuthService
     {
         Task<Result<LoginResponse>> LoginAsync(UserLoginRequest request);
+
+        Task<Result> ChangePasswordAsync(ChangePasswordRequest request, string userId);
     }
 }
