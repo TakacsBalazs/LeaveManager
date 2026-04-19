d98ee6d964f0d998517389c31a13740425d24fd7
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-19 19:20:22 +0200

    feat(backend): add CreateUserAsync to UserService

--- backend/LeaveManagerAPI/Services/UserService.cs
+++ backend/LeaveManagerAPI/Services/UserService.cs
@@ -1,6 +1,10 @@
 ﻿using LeaveManagerAPI.Common;
 using LeaveManagerAPI.Data;
+using LeaveManagerAPI.Extensions;
+using LeaveManagerAPI.Models;
+using LeaveManagerAPI.Models.Requests;
 using LeaveManagerAPI.Models.Responses;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace LeaveManagerAPI.Services
@@ -8,10 +12,56 @@ namespace LeaveManagerAPI.Services
     public class UserService : IUserService
     {
         private AppDbContext context;
-        public UserService(AppDbContext context)
+        private IServiceProvider serviceProvider;
+        private UserManager<User> userManager;
+        public UserService(AppDbContext context, IServiceProvider serviceProvider, UserManager<User> userManager)
         {
             this.context = context;
+            this.serviceProvider = serviceProvider;
+            this.userManager = userManager;
+        }
 
+
+        public async Task<Result<UserResponse>> CreateUserAsync(CreateUserRequest request)
+        {
+            var validate = await serviceProvider.ValidateRequestAsync<CreateUserRequest>(request);
+            if (!validate.IsSuccess) {
+                return Result<UserResponse>.Failure(validate.Errors);
+            }
+
+            var user = new User
+            {
+                FullName = request.FullName,
+                Email = request.Email,
+                UserName = request.Email
+            };
+
+            var userResult = await userManager.CreateAsync(user, request.Password);
+            if (!userResult.Succeeded) {
+                var errors = userResult.Errors.Select(x => x.Description).ToList();
+                return Result<UserResponse>.Failure(errors);
+            }
+
+            foreach (var role in request.Roles)
+            {
+                var roleResult = await userManager.AddToRoleAsync(user, role);
+                if (!roleResult.Succeeded)
+                {
+                    await userManager.DeleteAsync(user);
+
+                    var errors = roleResult.Errors.Select(x => x.Description).ToList();
+                    return Result<UserResponse>.Failure(errors);
+                }
+            }
+            var response = new UserResponse
+            {
+                Id = user.Id,
+                Fullname = user.FullName,
+                Email = user.Email,
+                Roles = request.Roles
+            };
+
+            return Result<UserResponse>.Success(response);
         }
 
         public async Task<Result<IEnumerable<UserResponse>>> GetUsersAsync()
