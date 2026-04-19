14bb150ca8aec07246698cdde7ee2898cc9cbffc
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-19 19:11:09 +0200

    feat(backend): create UserResponse

---
+++ backend/LeaveManagerAPI/Models/Responses/UserResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace LeaveManagerAPI.Models.Responses
+{
+    public class UserResponse
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Fullname { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public IEnumerable<string> Roles { get; set; } = new List<string>();
+    }
+}
