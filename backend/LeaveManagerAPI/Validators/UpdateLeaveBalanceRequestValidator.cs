452c8742dfb5b9a9767c5c2ec84ba077364c4333
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-19 20:51:05 +0200

    feat(backend): create UpdateLeaveBalanceRequestValidator

---
+++ backend/LeaveManagerAPI/Validators/UpdateLeaveBalanceRequestValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using LeaveManagerAPI.Models.Requests;
+
+namespace LeaveManagerAPI.Validators
+{
+    public class UpdateLeaveBalanceRequestValidator : AbstractValidator<UpdateLeaveBalanceRequest>
+    {
+        public UpdateLeaveBalanceRequestValidator() {
+            RuleFor(x => x.TotalDays).GreaterThanOrEqualTo(0);
+        }
+    }
+}
