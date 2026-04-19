36138900ce6e0573f129642facdc870080ca9878
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-19 19:22:19 +0200

    feat(backend): add IsRoleExist function and roles string array to UserRoles

--- backend/LeaveManagerAPI/Constants/UserRoles.cs
+++ backend/LeaveManagerAPI/Constants/UserRoles.cs
@@ -4,5 +4,11 @@
     {
         public const string Admin = "Admin";
         public const string Employee = "Employee";
+
+        public static readonly string[] All = { Admin, Employee };
+        public static bool IsRoleExist(string role)
+        {
+            return All.Contains(role);
+        }
     }
 }
