a40e6ed74dd5de9ca0900df2c6be51a8a44f02e6
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-19 19:16:17 +0200

    feat(backend): create UserController and add GetUsers

---
+++ backend/LeaveManagerAPI/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using LeaveManagerAPI.Constants;
+using LeaveManagerAPI.Models.Requests;
+using LeaveManagerAPI.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace LeaveManagerAPI.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private IUserService userService;
+        public UserController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        [Authorize(Roles = UserRoles.Admin)]
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var result = await userService.GetUsersAsync();
+            if(!result.IsSuccess)
+            {
+                return BadRequest(result.Errors);
+            }
+            return Ok(result.Data);
+        }
+    }
+}
