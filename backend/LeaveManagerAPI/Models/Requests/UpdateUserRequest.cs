e1706db812c57220232031cab2aa6b219d95161c
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-19 19:25:03 +0200

    feat(backend): create UpdateUserRequest

---
+++ backend/LeaveManagerAPI/Models/Requests/UpdateUserRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace LeaveManagerAPI.Models.Requests
+{
+    public class UpdateUserRequest
+    {
+        public string Fullname { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public IEnumerable<string> Roles { get; set; } = new List<string>();
+    }
+}
