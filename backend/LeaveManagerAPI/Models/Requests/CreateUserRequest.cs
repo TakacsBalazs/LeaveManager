4873a17fa2435b6a01a0d7e58871155e1e82f6b4
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-19 19:18:32 +0200

    feat(backend): create CreateUserRequest

---
+++ backend/LeaveManagerAPI/Models/Requests/CreateUserRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace LeaveManagerAPI.Models.Requests
+{
+    public class CreateUserRequest
+    {
+        public string FullName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+
+        public IEnumerable<string> Roles { get; set; } = new List<string>();
+    }
+}
