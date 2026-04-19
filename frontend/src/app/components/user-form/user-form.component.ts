daa62c152adda95435d06e47ab88a17e5b5efd4c
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-19 19:58:49 +0200

    feat(frontend): create user-form modal which contains edit and create form

---
+++ frontend/src/app/components/user-form/user-form.component.ts
@@ -0,0 +1,54 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { UserDto } from '../../models/user';
+import { OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-form',
+  imports: [ReactiveFormsModule],
+  templateUrl: './user-form.component.html',
+  styleUrl: './user-form.component.scss'
+})
+export class UserFormComponent implements OnInit{
+  @Input() user: UserDto | null = null;
+  @Input() errors: string[] = [];
+  @Output() closeForm = new EventEmitter<void>();
+  @Output() saveForm = new EventEmitter<UserDto>();
+
+  userForm!: FormGroup;
+
+  availableRoles = ['Admin', 'Employee']
+
+  ngOnInit(): void{
+    this.userForm = new FormGroup({
+      fullname: new FormControl(this.user?.fullname || '', [Validators.required]),
+      email: new FormControl(this.user?.email || '', [Validators.required, Validators.email]),
+      password: new FormControl('', this.user ? [] : [Validators.required, Validators.minLength(6)]),
+      roles: new FormControl(this.user?.roles || [], [Validators.required])
+    })
+  }
+
+  onSubmit(): void {
+    if(this.userForm.valid){
+      this.saveForm.emit(this.userForm.value);
+    } else {
+      this.userForm.markAllAsTouched();
+    }
+  }
+
+  onCancel(): void{
+    this.closeForm.emit();
+  }
+
+  onRoleChange(role: string, event: Event) {
+    const isChecked = (event.target as HTMLInputElement).checked;
+    
+    const currentRoles = this.userForm.get('roles')?.value as string[];
+  
+    if (isChecked) {
+      this.userForm.get('roles')?.setValue([...currentRoles, role]);
+    } else {
+      this.userForm.get('roles')?.setValue(currentRoles.filter(r => r !== role));
+    }
+  }
+}
