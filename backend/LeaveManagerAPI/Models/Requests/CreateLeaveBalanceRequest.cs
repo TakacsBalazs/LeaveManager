e02ecd78a0dacdd19fd81a66f3d89d9fb3a42db3
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-19 20:48:06 +0200

    feat(backend): create CreateLeaveBalanceRequest

---
+++ backend/LeaveManagerAPI/Models/Requests/CreateLeaveBalanceRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace LeaveManagerAPI.Models.Requests
+{
+    public class CreateLeaveBalanceRequest
+    {
+        public string UserId { get; set; } = string.Empty;
+
+        public int Year { get; set; }
+
+        public LeaveType Type { get; set; }
+
+        public int TotalDays { get; set; }
+
+        public int UsedDays { get; set; }
+    }
+}
