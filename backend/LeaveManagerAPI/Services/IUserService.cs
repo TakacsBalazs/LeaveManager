34772287859dce216bd544d7d77a6285c89d5cc8
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-19 19:12:37 +0200

    feat(backend): create IUserService and add GetUsersAsync

---
+++ backend/LeaveManagerAPI/Services/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using LeaveManagerAPI.Common;
+using LeaveManagerAPI.Models.Responses;
+
+namespace LeaveManagerAPI.Services
+{
+    public interface IUserService
+    {
+        Task<Result<IEnumerable<UserResponse>>> GetUsersAsync();
+    }
+}
