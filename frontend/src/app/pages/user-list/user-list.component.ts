0647598b5083524b874949c48cfc743466aa9c03
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-19 20:04:26 +0200

    feat(frontend): add user-form modal and the other which is needed to create and edit user

--- frontend/src/app/pages/user-list/user-list.component.ts
+++ frontend/src/app/pages/user-list/user-list.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import { UserDto } from '../../models/user';
+import { CreateUserRequest, UpdateUserRequest, UserDto } from '../../models/user';
 import { UserService } from '../../core/services/user.service';
+import { UserFormComponent } from '../../components/user-form/user-form.component';
 
 @Component({
   selector: 'app-user-list',
-  imports: [],
+  imports: [UserFormComponent],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.scss'
 })
 export class UserListComponent implements OnInit{
   data: UserDto[] = [];
   isLoading = true;
+  isModalOpen = false;
+  selectedUser: UserDto | null = null;
+  errors: string[] = [];
+  isConfirmOpen = false;
+  idToDelete: string | null = null;
 
   constructor(private userService: UserService) {
     
@@ -23,5 +29,59 @@ export class UserListComponent implements OnInit{
         this.isLoading = false;
       }
     })
+  }
+
+  openCreateModal(): void {
+    this.errors = [];
+    this.selectedUser = null;
+    this.isModalOpen = true;
+  }
+
+  openEditModal(user: UserDto): void {
+    this.selectedUser = user;
+    this.isModalOpen = true;
+  }
+
+  closeModal(): void {
+    this.isModalOpen = false;
+  }
+
+  handleUser(formData: any) {
+    if(this.selectedUser){
+      const user: UpdateUserRequest = {
+        fullname: formData.fullname,
+        email: formData.email,
+        roles: formData.roles
+      };
+
+      this.userService.updateUser(user, this.selectedUser.id).subscribe({
+        next: (resp) => {
+          const userInd = this.data.findIndex(x => x.id == this.selectedUser!.id);
+          this.data[userInd] = resp;
+          this.closeModal();
+        },
+        error: (err) => {
+          this.errors = err.error
+        }
+      })
+
+    } else {
+      const user: CreateUserRequest = {
+        fullname: formData.fullname,
+        email: formData.email,
+        roles: formData.roles,
+        password: formData.password
+      };
+
+      this.userService.createUser(user).subscribe({
+        next: (resp) => {
+          this.data.push(resp);
+          this.closeModal();
+        },
+        error: (err) => {
+          this.errors = err.error
+        }
+      })
+    }
   }
 }
