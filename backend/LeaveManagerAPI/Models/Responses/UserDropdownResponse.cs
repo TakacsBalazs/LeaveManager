c27538b8827c4fa9103bde7a018709f91d8d7e51
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-19 20:09:37 +0200

    feat(backend): create UserDrropdownResponse

---
+++ backend/LeaveManagerAPI/Models/Responses/UserDropdownResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace LeaveManagerAPI.Models.Responses
+{
+    public class UserDropdownResponse
+    {
+        public string Id { get; set; } = string.Empty;
+
+        public string Fullname { get; set; } = string.Empty;
+    }
+}
