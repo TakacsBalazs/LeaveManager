cab2a8de3574a3eecf34a885f45ccd76fe153bc7
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-19 20:48:17 +0200

    feat(backend): create CreateLeaveBalanceRequestValidator

---
+++ backend/LeaveManagerAPI/Validators/CreateLeaveBalanceRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using LeaveManagerAPI.Models.Requests;
+
+namespace LeaveManagerAPI.Validators
+{
+    public class CreateLeaveBalanceRequestValidator : AbstractValidator<CreateLeaveBalanceRequest>
+    {
+        public CreateLeaveBalanceRequestValidator()
+        {
+            RuleFor(x => x.TotalDays).GreaterThanOrEqualTo(0);
+            RuleFor(x => x.UsedDays).GreaterThanOrEqualTo(0).LessThanOrEqualTo(x => x.TotalDays);
+            RuleFor(x => x.Year).NotEmpty().GreaterThanOrEqualTo(DateTime.UtcNow.Year);
+            RuleFor(x => x.UserId).NotEmpty();
+        }
+    }
+}
