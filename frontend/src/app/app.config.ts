b48d46469382668a0347b60a43306244e931031b
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-20 18:42:54 +0200

    feat(frontend): setup the toast

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -5,7 +5,13 @@ import { routes } from './app.routes';
 
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(withInterceptors([authInterceptor]))]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(withInterceptors([authInterceptor])), provideAnimations(), provideToastr({
+    timeOut: 1500,
+    positionClass: 'toast-bottom-right',
+    preventDuplicates: true
+  })]
 };
