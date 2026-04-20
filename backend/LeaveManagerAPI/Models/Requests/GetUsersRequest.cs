a8bbd1e0ec36ce2dbd0fec1d20c843885ded1434
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-20 19:19:25 +0200

    feat(backend): create GetUsersRequest

---
+++ backend/LeaveManagerAPI/Models/Requests/GetUsersRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace LeaveManagerAPI.Models.Requests
+{
+    public class GetUsersRequest
+    {
+        public string? Fullname { get; set; }
+        public string? Email { get; set;}
+
+        public IEnumerable<string>? Roles { get; set; }
+    }
+}
