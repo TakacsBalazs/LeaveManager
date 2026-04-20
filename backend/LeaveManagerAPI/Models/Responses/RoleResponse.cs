358d196c70f1cd131cf9ad02f3379cd02a760e9a
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-20 19:36:13 +0200

    feat(backend): create RoleResponse

---
+++ backend/LeaveManagerAPI/Models/Responses/RoleResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace LeaveManagerAPI.Models.Responses
+{
+    public class RoleResponse
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+    }
+}
