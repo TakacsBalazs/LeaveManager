75fc32a268dbbfdb92f27a9982841104851ac7a3
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-20 19:05:14 +0200

    feat(backend): create GetAllLeaveBalancesRequest

---
+++ backend/LeaveManagerAPI/Models/Requests/GetAllLeaveBalancesRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace LeaveManagerAPI.Models.Requests
+{
+    public class GetAllLeaveBalancesRequest
+    {
+        public string? UserFullname { get; set; }
+
+        public int? Year { get; set; }
+
+        public LeaveType? Type { get; set; }
+    }
+}
