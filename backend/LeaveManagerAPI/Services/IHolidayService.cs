3a8c11072fcde2c36a1b8f28717dca51859189b8
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-29 22:11:09 +0200

    feat(backend): create IHolidayService and add GetAllHolidaysAsync

---
+++ backend/LeaveManagerAPI/Services/IHolidayService.cs
@@ -0,0 +1,11 @@
+﻿using LeaveManagerAPI.Common;
+using LeaveManagerAPI.Models.Requests;
+using LeaveManagerAPI.Models.Responses;
+
+namespace LeaveManagerAPI.Services
+{
+    public interface IHolidayService
+    {
+        Task<Result<IEnumerable<HolidayResponse>>> GetAllHolidaysAsync();
+    }
+}
