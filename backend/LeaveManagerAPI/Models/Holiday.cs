64b37b454f669a1a67a6e835c00573016ff89290
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-29 22:06:04 +0200

    feat(backend): create Holiday model

---
+++ backend/LeaveManagerAPI/Models/Holiday.cs
@@ -0,0 +1,11 @@
+﻿namespace LeaveManagerAPI.Models
+{
+    public class Holiday
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public DateOnly Date { get; set; }
+    }
+}
