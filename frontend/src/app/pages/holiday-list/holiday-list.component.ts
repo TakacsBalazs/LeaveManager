f2908becb1d125c01f85a839bcf9f099597fb60e
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-29 22:55:37 +0200

    feat(frontend): add holiday-form modal to holiday-list pages and handle the create holiday

--- frontend/src/app/pages/holiday-list/holiday-list.component.ts
+++ frontend/src/app/pages/holiday-list/holiday-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { HolidayResponse } from '../../models/holiday';
+import { CreateHolidayRequest, HolidayResponse } from '../../models/holiday';
 import { HolidayService } from '../../core/services/holiday.service';
+import { ToastrService } from 'ngx-toastr';
 import { HolidayFormComponent } from '../../components/holiday-form/holiday-form.component';
 import { ConfirmModalComponent } from '../../components/confirm-modal/confirm-modal.component';
 
@@ -13,8 +14,10 @@ import { ConfirmModalComponent } from '../../components/confirm-modal/confirm-mo
 export class HolidayListComponent implements OnInit{
   data: HolidayResponse[] = [];
   isLoading = true;
+  isModalOpen = false;
+  errors: string[] = [];
 
-  constructor(private holidayService: HolidayService){}
+  constructor(private holidayService: HolidayService, private toast: ToastrService){}
 
   ngOnInit(): void {
     this.holidayService.getAllHolidays().subscribe({
@@ -24,6 +27,29 @@ export class HolidayListComponent implements OnInit{
       },
       error: () => {
         this.isLoading = false;
+      }
+    });
+  }
+
+  openModal(){
+    this.errors = [];
+    this.isModalOpen = true;
+  }
+
+  closeModal(){
+    this.isModalOpen = false;
+  }
+
+  handleCreateHoliday(formData: CreateHolidayRequest){
+    this.holidayService.createHoliday(formData).subscribe({
+      next: (resp) => {
+        this.data.push(resp);
+        this.closeModal();
+        this.toast.success("Successfully created the holiday!", 'Success');
+      },
+      error: (err) => {
+        this.errors = err.error;
+        this.toast.error("Couldn't create the holiday!", 'Error');
       }
     });
   }
