165a727f2ff7e455ff04a7040721895e0cfa2643
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-29 22:11:58 +0200

    feat(backend): create HolidayService and add GetAllHolidaysAsync

---
+++ backend/LeaveManagerAPI/Services/HolidayService.cs
@@ -0,0 +1,29 @@
+﻿using LeaveManagerAPI.Common;
+using LeaveManagerAPI.Data;
+using LeaveManagerAPI.Models.Responses;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeaveManagerAPI.Services
+{
+    public class HolidayService : IHolidayService
+    {
+        private readonly AppDbContext context;
+
+        public HolidayService(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<Result<IEnumerable<HolidayResponse>>> GetAllHolidaysAsync()
+        {
+            var response = await context.Holidays.Select(x => new HolidayResponse
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Date = x.Date
+            }).ToListAsync();
+
+            return Result<IEnumerable<HolidayResponse>>.Success(response);
+        }
+    }
+}
