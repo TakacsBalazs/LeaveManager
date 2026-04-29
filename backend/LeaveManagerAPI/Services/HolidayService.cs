847a1ba5a4b9bed19a7ce68ec2bd71c5ae57b551
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-29 22:15:13 +0200

    feat(backend): add CreateHolidayAsync to HolidayService

--- backend/LeaveManagerAPI/Services/HolidayService.cs
+++ backend/LeaveManagerAPI/Services/HolidayService.cs
@@ -1,5 +1,8 @@
 ﻿using LeaveManagerAPI.Common;
 using LeaveManagerAPI.Data;
+using LeaveManagerAPI.Extensions;
+using LeaveManagerAPI.Models;
+using LeaveManagerAPI.Models.Requests;
 using LeaveManagerAPI.Models.Responses;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,10 +11,12 @@ namespace LeaveManagerAPI.Services
     public class HolidayService : IHolidayService
     {
         private readonly AppDbContext context;
+        private readonly IServiceProvider serviceProvider;
 
-        public HolidayService(AppDbContext context)
+        public HolidayService(AppDbContext context, IServiceProvider serviceProvider)
         {
             this.context = context;
+            this.serviceProvider = serviceProvider;
         }
 
         public async Task<Result<IEnumerable<HolidayResponse>>> GetAllHolidaysAsync()
@@ -24,6 +29,38 @@ namespace LeaveManagerAPI.Services
             }).ToListAsync();
 
             return Result<IEnumerable<HolidayResponse>>.Success(response);
+        }
+
+        public async Task<Result<HolidayResponse>> CreateHolidayAsync(CreateHolidayRequest request)
+        {
+            var validate = await serviceProvider.ValidateRequestAsync<CreateHolidayRequest>(request);
+            if(!validate.IsSuccess)
+            {
+                return Result<HolidayResponse>.Failure(validate.Errors);
+            }
+
+            var hasSameDate = await context.Holidays.AnyAsync(x => x.Date ==  request.Date);
+            if(hasSameDate)
+            {
+                return Result<HolidayResponse>.Failure("There is a same holiday!");
+            }
+
+            var holiday = new Holiday
+            {
+                Name = request.Name,
+                Date = request.Date
+            };
+
+            context.Holidays.Add(holiday);
+            await context.SaveChangesAsync();
+
+            var response = new HolidayResponse
+            {
+                Id = holiday.Id,
+                Name = request.Name,
+                Date = request.Date
+            };
+            return Result<HolidayResponse>.Success(response);
         }
     }
 }
