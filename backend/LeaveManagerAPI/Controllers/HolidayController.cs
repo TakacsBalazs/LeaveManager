1a732cd6c7330e12f8244079e8a93e78443ab2c5
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-29 22:12:45 +0200

    feat(backend): create HolidayController and add GetAllHolidays

---
+++ backend/LeaveManagerAPI/Controllers/HolidayController.cs
@@ -0,0 +1,31 @@
+﻿using LeaveManagerAPI.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagerAPI.Controllers
+{
+    [Route("api/holidays")]
+    [ApiController]
+    public class HolidayController : ControllerBase
+    {
+        private readonly IHolidayService holidayService;
+
+        public HolidayController(IHolidayService holidayService)
+        {
+            this.holidayService = holidayService;
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> GetAllHolidays()
+        {
+            var result = await holidayService.GetAllHolidaysAsync();
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result.Errors);
+            }
+
+            return Ok(result.Data);
+        }
+    }
+}
