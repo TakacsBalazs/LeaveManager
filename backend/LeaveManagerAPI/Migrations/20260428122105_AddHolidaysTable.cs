1bfb2e62e5c693cfd147ae1c95dabca9785a220f
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-29 22:08:56 +0200

    feat(backend): create AddHolidaysTable migration

---
+++ backend/LeaveManagerAPI/Migrations/20260428122105_AddHolidaysTable.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LeaveManagerAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHolidaysTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Holidays",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Date = table.Column<DateOnly>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Holidays", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Holidays_Date",
+                table: "Holidays",
+                column: "Date",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Holidays");
+        }
+    }
+}
