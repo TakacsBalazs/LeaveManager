bc0fdd864227044a4d4bdabc03d1f74d274fe8b0
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-04-29 22:14:06 +0200

    feat(backend): create CreateHolidayRequestValidator

---
+++ backend/LeaveManagerAPI/Validators/CreateHolidayRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using LeaveManagerAPI.Models.Requests;
+
+namespace LeaveManagerAPI.Validators
+{
+    public class CreateHolidayRequestValidator : AbstractValidator<CreateHolidayRequest>
+    {
+        public CreateHolidayRequestValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().MaximumLength(100);
+            RuleFor(x => x.Date).NotEmpty();
+        }
+    }
+}
