5b8c8e4ae8513ca8136a9b91c9b5cf477920e085
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-05-17 21:26:13 +0200

    feat(backend): create NotificationResponse

---
+++ backend/LeaveManagerAPI/Models/Responses/NotificationResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace LeaveManagerAPI.Models.Responses
+{
+    public class NotificationResponse
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Message { get; set; } = string.Empty;
+
+        public bool IsRead { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
