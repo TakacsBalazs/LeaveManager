da7317181b3a69cb00f8a2322472d990229add3e
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-05-17 21:28:37 +0200

    feat(backend): create NotificationService and add GetUserAllNotificationAsync

---
+++ backend/LeaveManagerAPI/Services/NotificationService.cs
@@ -0,0 +1,34 @@
+﻿using LeaveManagerAPI.Common;
+using LeaveManagerAPI.Data;
+using LeaveManagerAPI.Hubs;
+using LeaveManagerAPI.Models.Responses;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeaveManagerAPI.Services
+{
+    public class NotificationService : INotificationService
+    {
+        private readonly AppDbContext context;
+
+        public NotificationService(AppDbContext context, IHubContext<LeaveHub, ILeaveClient> hubContext)
+        {
+            this.context = context;
+        }
+
+        public async Task<Result<IEnumerable<NotificationResponse>>> GetUserAllNotificationAsync(string userId)
+        {
+            var response = await context.Notifications.Where(x => x.UserId == userId).OrderByDescending(x => x.CreatedAt)
+                .Select(x => new NotificationResponse
+                {
+                    Id = x.Id,
+                    Title = x.Title,
+                    Message = x.Message,
+                    IsRead = x.IsRead,
+                    CreatedAt = x.CreatedAt
+                }).ToListAsync();
+
+            return Result<IEnumerable<NotificationResponse>>.Success(response);
+        }
+    }
+}
