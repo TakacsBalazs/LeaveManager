d383169adfb641f61153f4ede699ee0f7f82c533
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-05-17 21:27:53 +0200

    feat(backend): create INotificationService and add GetUserAllNotificationAsync

---
+++ backend/LeaveManagerAPI/Services/INotificationService.cs
@@ -0,0 +1,11 @@
+﻿using LeaveManagerAPI.Common;
+using LeaveManagerAPI.Models.Responses;
+
+namespace LeaveManagerAPI.Services
+{
+    public interface INotificationService
+    {
+
+        Task<Result<IEnumerable<NotificationResponse>>> GetUserAllNotificationAsync(string userId);
+    }
+}
