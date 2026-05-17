7bcb42a07afd61d9c795e378d6558b34181fd3cc
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-05-17 23:39:49 +0200

    feat(backend): create ILeaveClient add ReceiveNotification

---
+++ backend/LeaveManagerAPI/Hubs/ILeaveClient.cs
@@ -0,0 +1,9 @@
+﻿using LeaveManagerAPI.Models.Responses;
+
+namespace LeaveManagerAPI.Hubs
+{
+    public interface ILeaveClient
+    {
+        Task ReceiveNotification(NotificationResponse notification);
+    }
+}
