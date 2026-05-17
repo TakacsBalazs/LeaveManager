9763336659ecb9fba3c6cff3b7b1aa034bf9e080
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-05-17 21:32:30 +0200

    feat(backend): create NotificationController and add GetUserAllNotification

---
+++ backend/LeaveManagerAPI/Controllers/NotificationController.cs
@@ -0,0 +1,30 @@
+﻿using LeaveManagerAPI.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace LeaveManagerAPI.Controllers
+{
+    [Route("api/notifications")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        private readonly INotificationService notificationService;
+
+        public NotificationController(INotificationService notificationService)
+        {
+            this.notificationService = notificationService;
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> GetUserAllNotification()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier)!;
+            var result = await notificationService.GetUserAllNotificationAsync(userId);
+            
+            return Ok(result.Data);
+        }
+    }
+}
