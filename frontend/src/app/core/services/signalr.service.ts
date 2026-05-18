e54926b41152e2e5aa689d63de00a91215fd4189
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-05-18 12:23:35 +0200

    feat(frontend): create SignalrService and add startConncetion and onNewNotification and subscribe to logoutEvent and stop the connection

---
+++ frontend/src/app/core/services/signalr.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import * as signalR from '@microsoft/signalr'
+import { NotificationResponse } from '../../models/notification';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SignalrService {
+  private hubConnection: signalR.HubConnection | undefined ;
+
+  constructor(private authService: AuthService){
+    this.authService.logoutEvent$.subscribe(() => {
+      this.hubConnection?.stop();
+    });
+  }
+
+  public startConnection() {
+    this.hubConnection = new signalR.HubConnectionBuilder().withUrl('https://localhost:7171/leave-hub', {
+      accessTokenFactory: () => localStorage.getItem('jwt_token') ?? ''
+    }).withAutomaticReconnect().build();
+
+    this.hubConnection.start()
+  }
+
+  public onNewNotification(callback: (newNotification: NotificationResponse) => void) {
+    this.hubConnection?.on('ReceiveNotification', (notification) => {
+      callback(notification);
+    })
+  }
+}
