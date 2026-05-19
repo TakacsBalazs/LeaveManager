29a4f6c5ee0f21614a136b3672335e7f516b1db7
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-05-19 12:05:03 +0200

    feat(backend): create GetLeaveRequestCalendarRequest

---
+++ backend/LeaveManagerAPI/Models/Requests/GetLeaveRequestCalendarRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace LeaveManagerAPI.Models.Requests
+{
+    public class GetLeaveRequestCalendarRequest
+    {
+        public DateOnly? StartDate { get; set; }
+
+        public DateOnly? EndDate { get; set; }
+    }
+}
