716673bf762ab4860ef9844c982c91282800a4ae
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-05-19 12:04:38 +0200

    feat(backend): create LeaveRequestCalendarResponse

---
+++ backend/LeaveManagerAPI/Models/Responses/LeaveRequestCalendarResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace LeaveManagerAPI.Models.Responses
+{
+    public class LeaveRequestCalendarResponse
+    {
+        public int Id { get; set; }
+
+        public LeaveType Type { get; set; }
+
+        public DateOnly StartDate { get; set; }
+
+        public DateOnly EndDate { get; set; }
+
+        public LeaveRequestStatus Status { get; set; }
+
+        public string RequesterName { get; set; } = string.Empty;
+    }
+}
