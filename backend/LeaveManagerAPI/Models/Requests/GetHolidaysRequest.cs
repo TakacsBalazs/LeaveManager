eace3fac1795fce3ece6ef68ca23749c3ac9c70f
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-05-20 19:18:44 +0200

    feat(backend): create GetHolidayRequest

---
+++ backend/LeaveManagerAPI/Models/Requests/GetHolidaysRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace LeaveManagerAPI.Models.Requests
+{
+    public class GetHolidaysRequest
+    {
+        public DateOnly? MinDate { get; set; }
+
+        public DateOnly? MaxDate { get; set; }
+    }
+}
