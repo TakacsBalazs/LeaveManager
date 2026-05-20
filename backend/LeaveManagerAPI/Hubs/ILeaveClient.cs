cd5eea615810950c055d0dc48c509c639e538af9
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-05-20 19:12:24 +0200

    feat(backend): add LeaveRequestChanged to ILeaveClient

--- backend/LeaveManagerAPI/Hubs/ILeaveClient.cs
+++ backend/LeaveManagerAPI/Hubs/ILeaveClient.cs
@@ -1,9 +1,12 @@
-﻿using LeaveManagerAPI.Models.Responses;
+﻿using LeaveManagerAPI.Models;
+using LeaveManagerAPI.Models.Responses;
 
 namespace LeaveManagerAPI.Hubs
 {
     public interface ILeaveClient
     {
         Task ReceiveNotification(NotificationResponse notification);
+
+        Task LeaveRequestChanged(int id, LeaveRequestStatus status);
     }
 }
