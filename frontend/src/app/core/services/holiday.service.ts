ee4b852fe59fb74e1e104241a8ea26e41e3d0319
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-05-20 19:22:41 +0200

    feat(backend): set up the filter part for getAllHolidays in HolidayService

--- frontend/src/app/core/services/holiday.service.ts
+++ frontend/src/app/core/services/holiday.service.ts
@@ -2,8 +2,9 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { Observable } from 'rxjs';
-import { CreateHolidayRequest, HolidayResponse } from '../../models/holiday';
+import { CreateHolidayRequest, FilterHoliday, HolidayResponse } from '../../models/holiday';
 import { MessageResponse } from '../../models/api-responses';
+import { buildCleanParams } from '../utils/http.utils';
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +14,10 @@ export class HolidayService {
 
   constructor(private http: HttpClient) { }
 
-  getAllHolidays(): Observable<HolidayResponse[]>{
-    return this.http.get<HolidayResponse[]>(this.apiUrl);
+  getAllHolidays(filter: FilterHoliday | null): Observable<HolidayResponse[]>{
+    const cleanParams = buildCleanParams(filter);
+
+    return this.http.get<HolidayResponse[]>(this.apiUrl, {params: cleanParams});
   }
 
   createHoliday(request: CreateHolidayRequest): Observable<HolidayResponse>{
