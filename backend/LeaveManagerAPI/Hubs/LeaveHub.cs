1cf7220571a5100421548cc3b3bb5c8e8d7d6dba
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-05-20 19:13:49 +0200

    feat(backend): override OnConnectedAsync and add admin to admin group in LeaveHub

--- backend/LeaveManagerAPI/Hubs/LeaveHub.cs
+++ backend/LeaveManagerAPI/Hubs/LeaveHub.cs
@@ -5,5 +5,14 @@ namespace LeaveManagerAPI.Hubs
 {
     public class LeaveHub : Hub<ILeaveClient>
     {
+        public override async Task OnConnectedAsync()
+        {
+            if (Context.User.IsInRole(UserRoles.Admin))
+            {
+                await Groups.AddToGroupAsync(Context.ConnectionId, UserRoles.Admin);
+            }
+
+            await base.OnConnectedAsync();
+        }
     }
 }
