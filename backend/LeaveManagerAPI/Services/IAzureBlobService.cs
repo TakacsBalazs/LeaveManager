b32cfd308303001ef85a56e3ac4ddf21ec54a916
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-05-27 18:59:48 +0200

    feat(backend): create IAzureBlobService and add UploadAsync

---
+++ backend/LeaveManagerAPI/Services/IAzureBlobService.cs
@@ -0,0 +1,9 @@
+﻿using LeaveManagerAPI.Common;
+
+namespace LeaveManagerAPI.Services
+{
+    public interface IAzureBlobService
+    {
+        Task<Result<string>> UploadAsync(Stream stream, string fileName, string folderName, string contentType);
+    }
+}
