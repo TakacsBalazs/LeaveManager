95ce751d0ff91eafbc01066172e1a06876a612a1
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-05-27 19:12:37 +0200

    feat(backend): create UploadProfilePictureRequest

---
+++ backend/LeaveManagerAPI/Models/Requests/UploadProfilePictureRequest.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace LeaveManagerAPI.Models.Requests
+{
+    public class UploadProfilePictureRequest
+    {
+        public IFormFile File { get; set; }
+    }
+}
