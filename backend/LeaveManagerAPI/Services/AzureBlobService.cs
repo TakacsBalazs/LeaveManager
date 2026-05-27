45f7f2128511d1c9d9903fb22401d62c3edd5600
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-05-27 19:00:11 +0200

    feat(backend): create AzureBlobService and add UploadAsync

---
+++ backend/LeaveManagerAPI/Services/AzureBlobService.cs
@@ -0,0 +1,33 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Sas;
+using LeaveManagerAPI.Common;
+
+namespace LeaveManagerAPI.Services
+{
+    public class AzureBlobService : IAzureBlobService
+    {
+        private readonly BlobServiceClient blobServiceClient;
+        public AzureBlobService(BlobServiceClient blobServiceClient)
+        {
+            this.blobServiceClient = blobServiceClient;
+        }
+
+        public async Task<Result<string>> UploadAsync(Stream stream, string fileName, string folderName, string contentType)
+        {
+            var container = blobServiceClient.GetBlobContainerClient(folderName);
+
+            await container.CreateIfNotExistsAsync();
+
+            string blobPath = $"{Guid.NewGuid()}_{fileName}";
+
+            var blobClient = container.GetBlobClient(blobPath);
+
+            await blobClient.UploadAsync(stream, new BlobUploadOptions{
+                HttpHeaders = new BlobHttpHeaders { ContentType = contentType }
+            });
+
+            return Result<string>.Success(blobPath);
+        }
+    }
+}
