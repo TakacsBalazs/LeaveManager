88d69db08e99d4bedbcf89fc1089d0e906a3aec3
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-05-27 19:22:00 +0200

    feat(frontend): show the profile picture in dashboard page

--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DashboardDto } from '../../models/dashboard';
 import { DashboardService } from '../../core/services/dashboard.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-dashboard',
@@ -13,13 +14,17 @@ export class DashboardComponent implements OnInit{
   data: DashboardDto | null = null;
   isLoading = true;
 
-  constructor(private dashboardService: DashboardService) {}
+  constructor(private dashboardService: DashboardService, private toast: ToastrService) {}
 
   ngOnInit(): void {
     this.dashboardService.getDashboard().subscribe({
       next: (data) => {
         this.data = data;
         this.isLoading = false;
+      },
+      error: () => {
+        this.isLoading = false;
+        this.toast.error('There was an error loading the data. Please try again later.', 'Error');
       }
     });
   }
